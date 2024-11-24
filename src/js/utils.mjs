04d6cb8f02dec452d2c1eb429cafedd4fe332ae7
vbisso <valeriabisso19@gmail.com>
2024-11-23 21:31:04 -0700

    homepage

---
+++ src/js/utils.mjs
@@ -0,0 +1,41 @@
+export function loadTemplate(path) {
+  return async function () {
+    const res = await fetch(path);
+    if (res.ok) {
+      const html = await res.text();
+      return html;
+    }
+  };
+}
+export async function loadHeaderFooter() {
+  const headerTemplateFn = loadTemplate("/partials/header.html");
+  const footerTemplateFn = loadTemplate("/partials/footer.html");
+
+  const headerEl = document.querySelector("#main-header");
+  const footerEl = document.querySelector("#main-footer");
+
+  if (headerEl) {
+    await renderWithTemplate(headerTemplateFn, headerEl);
+  }
+
+  if (footerEl) {
+    await renderWithTemplate(footerTemplateFn, footerEl);
+  }
+}
+export async function renderWithTemplate(
+  templateFn,
+  parentElement,
+  data,
+  callback,
+  position = "afterbegin",
+  clear = true
+) {
+  if (clear) {
+    parentElement.innerHTML = "";
+  }
+  const htmlString = await templateFn(data);
+  parentElement.insertAdjacentHTML(position, htmlString);
+  if (callback) {
+    callback(data);
+  }
+}
