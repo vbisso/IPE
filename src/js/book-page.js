74b9903f478f77f00a0b5ffaa30a7144209171d9
vbisso <valeriabisso19@gmail.com>
2024-12-11 21:54:36 -0700

    total cart

--- src/js/book-page.js
+++ src/js/book-page.js
@@ -3,3 +3,12 @@ import bookPage from "./bookPage.mjs";
 
 loadHeaderFooter();
 bookPage();
+/*
+window.addEventListener("beforeunload", (event) => {
+  event.preventDefault();
+});
+
+window.addEventListener("unload", () => {
+  localStorage.clear();
+});
+*/
--- src/js/bookPage.mjs
+++ src/js/bookPage.mjs
@@ -1,7 +1,8 @@
-import { getLocalStorage, formatDate } from "./utils.mjs";
+import { getLocalStorage, formatDate, setLocalStorage } from "./utils.mjs";
 
 function rederBookDetails(item) {
   document.querySelector("#book-details-img").src = item[0].img;
+  document.querySelector("#book-details-img").alt = item[0].model;
   document.querySelector("#book-details-model").innerText = item[0].model;
   document.querySelector("#book-details-option").innerText =
     item[0].selectedOption + " " + item[0].type + " Rental";
@@ -15,20 +16,62 @@ function setBookDate() {
   const formattedDate = formatDate(date);
   document.querySelector("#book-details-date").innerHTML = formattedDate;
 }
+function setQuantity() {
+  const selectedQuantity = document.querySelector("#quantity").value;
+  setLocalStorage("so-quantity", selectedQuantity);
+  updateQuantity();
+  displayTotal();
+}
+
+function updateQuantity() {
+  const quantity = getLocalStorage("so-quantity");
+  //console.log(quantity);
+  if (quantity >= "1") {
+    document.querySelector(".book-confirmation").classList.remove("hide");
+  } else {
+    document.querySelector(".book-confirmation").classList.add("hide");
+  }
+}
+
+function displayTotal() {
+  const bookItems = getLocalStorage("so-book");
+
+  if (bookItems) {
+    const subtotal = calculateSubtotal(bookItems[0]);
+    document.querySelector("#sub-total").innerText =
+      "Subtotal: " + "$" + subtotal;
+
+    const tax = subtotal * 0.06; //6% tax
+    document.querySelector("#taxes").innerText = "Tax: " + "$" + tax;
+
+    const total = tax + subtotal;
+    document.querySelector("#total").innerText = "Total: " + "$" + total;
+  }
+}
+
+function calculateSubtotal(bookItem) {
+  const quantity = getLocalStorage("so-quantity");
+  const subtotal = quantity * bookItem.selectedPrice;
+  return subtotal;
+}
 
 export default function bookPage() {
   const bookItems = getLocalStorage("so-book");
 
   if (!bookItems) {
-    return;
+    window.location.href = "../product-list/index.html";
+  }
+
+  if (bookItems[0].selectedOption === "Half Day") {
+    document.querySelector(".half-day-options-form").classList.remove("hide");
   }
 
   setBookDate();
   rederBookDetails(bookItems);
 
+  document.querySelector("#quantity").addEventListener("change", setQuantity);
+
   //const total = calculateTotal(cartItems);
 
   //displayCartTotal(total);
 }
-
-function displayHalfDayOptions() {}
